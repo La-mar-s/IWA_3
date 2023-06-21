f6f21bb0f0839e93e8fc48d678f52c28d8d259fe
La-mar-s <sony.lamar.1995@gmail.com>
2023-06-21 22:15:52 +0200

    finished IWA_3

---
+++ Challenge_2/johannes.js
@@ -0,0 +1,6 @@
+let firstname = "Johannes"
+let surname = "Potgieter"
+const role = "Intern"
+
+let display= firstname + " " + surname + " (" + role + ")"
+document.querySelector('#johannes').innerText = display
